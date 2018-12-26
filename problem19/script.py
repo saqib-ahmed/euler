621c4eaf1a7e92a247137fb5c9427d7cdc6b305c
saqibahmed515 <saqibahmed515@gmail.com>
2018-12-26 21:31:20 +0500

    Solved problem 19

---
+++ problem19/script.py
@@ -0,0 +1,9 @@
+import datetime
+result = 0
+
+for year in range(1901, 2001):
+    for month in range(1, 13):
+        if datetime.datetime(year, month, 1).weekday() == 6:
+            result += 1
+
+print(result)
