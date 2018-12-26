093374b452a9158c1e425ec8879e6a53b67cf902
saqibahmed515 <saqibahmed515@gmail.com>
2018-12-26 22:52:06 +0500

    Added stackoverflow link for efficient factorisation algorithm

--- problem21/script.py
+++ problem21/script.py
@@ -2,6 +2,7 @@ from functools import reduce
 import numpy as np
 
 
+# https://stackoverflow.com/questions/6800193/what-is-the-most-efficient-way-of-finding-all-the-factors-of-a-number-in-python
 def factors(n):
     return set(reduce(list.__add__,
                       ([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0)))
