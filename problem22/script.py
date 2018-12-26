adcf5199e8247dd13d5c0459437041c2907b6a55
saqibahmed515 <saqibahmed515@gmail.com>
2018-12-27 00:05:10 +0500

    Updated problem 22 solution

--- problem22/script.py
+++ problem22/script.py
@@ -1,21 +1,14 @@
 
-def letter_index(let):
-    return ord(let) - 96
-
-
 file = open("./p022_names.txt").read()
-
 arr = [i for i in file.split(',')]
-
 arr.sort()
-
 total_sum = 0
 
 for index, val in enumerate(arr):
     individual_sum = 0
     for letter in val:
         if letter != "\"":
-            individual_sum += letter_index(letter.lower())
+            individual_sum += ord(letter.lower())-96
     total_sum += (individual_sum * (index + 1))
 
 print(total_sum)
